5d27f89be37fa6364f80404d2baf4e5bf5778253
matsu7874 <mtsmtkmt@gmail.com>
2022-10-16 02:13:29 +0900

    ImageMagickでsvgからgifを作成するワンライナーを追記

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-cargo build --release && RUST_LOG=debug cargo run --release --bin judge
+cargo build --release && RUST_LOG=info cargo run --release --bin judge > tmp/kifu.txt
+
+rm tmp/TableturfBattle_*.svg tmp/TableturfBattle_*.png
+
+RUST_LOG=info cargo run --release --bin record_player < tmp/kifu.txt
+convert -density 50 tmp/TableturfBattle_*.svg tmp/TableturfBattle_%04d.png
+convert -delay 50 -loop 0 tmp/TableturfBattle_*.png tmp/TableturfBattle.gif
