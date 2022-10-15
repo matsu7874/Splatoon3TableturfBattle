6c65e1b31657ccc8c0c260ffd3c471ccbb6c4dcc
matsu7874 <mtsmtkmt@gmail.com>
2022-10-15 23:14:13 +0900

    棋譜を出力するように修正

--- judge/src/main.rs
+++ judge/src/main.rs
@@ -1,11 +1,13 @@
 use log::{debug, info};
 use rand::{rngs::ThreadRng, seq::SliceRandom};
 use serde::{Deserialize, Serialize};
+use std::{io::prelude::*, path::Path};
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
 use std::{collections::HashMap, process::Child};
-use std::{io::prelude::*};
-use tableturfbattle::{Action, Card, CardShape, Environment, Field, State};
+use tableturfbattle::{
+    Action, Card, CardShape, CardId, Environment, Field, MulliganAction, PlayerId, State,
+};
 
 #[derive(Serialize, Deserialize)]
 struct CardJson {
@@ -21,8 +23,8 @@ impl CardJson {
         Card::new(self.id, &self.name, self.cost, shape)
     }
 }
-fn load_card_catalog() -> serde_json::Result<Vec<Card>> {
-    let text = std::fs::read_to_string("card_catalog.json").unwrap();
+fn load_card_catalog(card_catalog_path: &str) -> serde_json::Result<Vec<Card>> {
+    let text = std::fs::read_to_string(card_catalog_path).unwrap();
     let raw_cards = serde_json::from_str::<Vec<CardJson>>(&text).unwrap();
     let mut cards = vec![];
     for c in raw_cards {
@@ -50,6 +52,21 @@ fn lunch_bots(commands: &[&str]) -> Vec<Child> {
     }
     bot_processes
 }
+fn read_names(bot_processes: &mut Vec<Child>) -> Vec<String> {
+    let mut names = vec![];
+    for (player_id, bot_process) in bot_processes.iter_mut().enumerate() {
+        let stdout = bot_process.stdout.as_mut().expect("");
+        let mut reader = BufReader::new(stdout);
+        let mut s = String::new();
+        match reader.read_line(&mut s) {
+            Err(why) => panic!("couldn't read player stdout: {}", why),
+            Ok(_) => debug!("player {} name: {}", player_id, s),
+        };
+        let name = s.trim().to_string();
+        names.push(name);
+    }
+    names
+}
 fn format_initial_input(env: &Environment, cards: &[Card], field: &Field) -> String {
     let cards_info = cards
         .iter()
@@ -63,10 +80,12 @@ fn format_initial_input(env: &Environment, cards: &[Card], field: &Field) -> Str
         .join("\n");
 
     format!(
-        "{deck_size} {hand_size} {max_turn}\n{stage_size_y} {stage_size_x}\n{stage_shape}\n{number_of_cards}\n{cards_info}\n",
+        "{player_size} {deck_size} {hand_size} {max_turn} {is_deplicated_pick_enabled}\n{stage_size_y} {stage_size_x}\n{stage_shape}\n{number_of_cards}\n{cards_info}\n",
+        player_size = env.player_size,
         deck_size = env.deck_size,
         hand_size = env.hand_size,
         max_turn = env.max_turn,
+        is_deplicated_pick_enabled = if env.is_deplicated_pick_enabled{"1"}else{"0"},
         stage_size_y = field.shape.height,
         stage_size_x = field.shape.width,
         stage_shape = field.shape,
@@ -83,8 +102,9 @@ fn print_initial_input(bot_processes: &mut Vec<Child>, initial_input: &str) {
         }
         stdin.flush().unwrap_or(());
     }
+    print!("{}", initial_input); //末尾に改行が含まれる文字列であるため
 }
-fn read_decks(bot_processes: &mut Vec<Child>, rng: &mut ThreadRng) -> Vec<Vec<usize>> {
+fn read_decks(bot_processes: &mut Vec<Child>) -> Vec<Vec<usize>> {
     let mut decks = vec![];
     for (player_id, bot_process) in bot_processes.iter_mut().enumerate() {
         let stdout = bot_process.stdout.as_mut().expect("");
@@ -103,22 +123,40 @@ fn read_decks(bot_processes: &mut Vec<Child>, rng: &mut ThreadRng) -> Vec<Vec<us
                     .expect("botを完全に信用する。TODO:ここは入力の検証が必要。")
             })
             .collect();
-        deck.shuffle(rng);
-
+        println!(
+            "{}",
+            deck.iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
         decks.push(deck);
     }
     decks
 }
-fn check_mulligan(
+fn shuffle_and_mulligan(
     env: &Environment,
     bot_processes: &mut Vec<Child>,
-    decks: &mut Vec<Vec<usize>>,
+    decks: &Vec<Vec<usize>>,
     rng: &mut ThreadRng,
-) {
+) -> Vec<Vec<usize>> {
+    let mut shuffled_decks = vec![];
     for (player_id, bot_process) in bot_processes.iter_mut().enumerate() {
+        let mut deck = decks[player_id].clone();
+        deck.shuffle(rng);
+        // デッキの順番を記録する
+        println!(
+            "{}",
+            deck.iter()
+                .map(|card_id| { card_id.to_string() })
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+        shuffled_decks.push(deck);
+
         let hands = format!(
             "{}\n",
-            decks[player_id][0..env.hand_size]
+            shuffled_decks[player_id][0..env.hand_size]
                 .iter()
                 .map(|v| v.to_string())
                 .collect::<Vec<String>>()
@@ -137,11 +175,14 @@ fn check_mulligan(
             Err(why) => panic!("couldn't read player stdout: {}", why),
             Ok(_) => debug!("player {} mulligan: {}", player_id, s),
         };
-        if s == "MULLIGAN" {
+        let action = MulliganAction::from(s.as_str());
+        println!("{}", action);
+        if action == MulliganAction::Mulligan {
             debug!("player {} mulliganed", player_id);
-            decks[player_id].shuffle(rng);
+            shuffled_decks[player_id].shuffle(rng);
         }
     }
+    shuffled_decks
 }
 fn format_turn_input(
     field: &Field,
@@ -209,6 +250,7 @@ fn game_loop(
         stdin.flush().unwrap_or(());
 
         let action = read_action(bot_process, player_id);
+        println!("{}", action);
         actions.push(action);
     }
 
@@ -218,6 +260,7 @@ fn game_loop(
 fn exec_game(env: &Environment, cards: &[Card], field: &Field, commands: &[&str]) -> GameInfo {
     let mut rng = rand::thread_rng();
     let mut bot_processes = lunch_bots(commands);
+    let names = read_names(&mut bot_processes);
 
     let initial_input = format_initial_input(env, cards, field);
     print_initial_input(&mut bot_processes, &initial_input);
@@ -227,11 +270,22 @@ fn exec_game(env: &Environment, cards: &[Card], field: &Field, commands: &[&str]
         card_catalog.insert(card.id, card);
     }
 
-    let mut decks = read_decks(&mut bot_processes, &mut rng);
+    println!("{}", names.join("\n"));
 
-    check_mulligan(env, &mut bot_processes, &mut decks, &mut rng);
+    let decks = read_decks(&mut bot_processes);
+    let shuffled_decks = shuffle_and_mulligan(env, &mut bot_processes, &decks, &mut rng);
+    // デッキの順番を記録する
+    for deck in shuffled_decks.iter() {
+        println!(
+            "{}",
+            deck.iter()
+                .map(|card_id| { card_id.to_string() })
+                .collect::<Vec<String>>()
+                .join(" ")
+        )
+    }
 
-    let mut state = State::new(env, &card_catalog, field, &decks);
+    let mut state = State::new(env, &card_catalog, field, &shuffled_decks);
     // 毎ターンの繰り返し処理
     while !state.is_done(env) {
         game_loop(env, field, &card_catalog, &mut bot_processes, &mut state)
@@ -264,30 +318,26 @@ fn main() {
     // 2つのプログラムと情報の受け渡しを行いゲームを進めるプログラム
     env_logger::init();
 
-    let env = Environment::new(2, 15, 4, 12);
-    let cards = load_card_catalog().expect("JSON読み込みはうまくいく");
+    // let env = Environment::new(2, 15, 4, 12, false);
+    let env = Environment::new(2, 10, 4, 6, true);
+    let cards = load_card_catalog("resources/card_catalog_sample.json").expect("JSON読み込みはうまくいく");
     // TODO: 別のフィールドも使えるようにする
     let field = Field::default();
-    let mut total_wins = vec![0; 2];
-    for i in 0..3 {
-        let result = exec_game(
-            &env,
-            &cards,
-            &field,
-            &["target/release/bot", "target/release/bot"],
+    let result = exec_game(
+        &env,
+        &cards,
+        &field,
+        &["target/release/bot", "target/release/bot"],
+    );
+    if let Some(winner) = result.winner {
+        info!(
+            "winner:{}\tplayer0_square:{}\tplayer1_square:{}",
+            winner, result.n_squares[0], result.n_squares[1]
         );
-        if let Some(winner) = result.winner {
-            total_wins[winner] += 1;
-            info!(
-                "game:{}\twinner:{}\tplayer0_square:{}\tplayer1_square:{}",
-                i, winner, result.n_squares[0], result.n_squares[1]
-            );
-        } else {
-            info!(
-                "game:{}\twinner:none\tplayer0_square:{}\tplayer1_square:{}",
-                i, result.n_squares[0], result.n_squares[1]
-            );
-        }
+    } else {
+        info!(
+            "winner:none\tplayer0_square:{}\tplayer1_square:{}",
+            result.n_squares[0], result.n_squares[1]
+        );
     }
-    println!("\ntotal_wins: {:?}", total_wins);
 }
